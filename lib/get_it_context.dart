0f0236d498b828eb5ad7598e9ee6d97a434d65f2
Kira Resari <kira@tri-tail.com>
2024-02-03 20:00:19 +0100

    Fixed GetIt context and added test to make sure it works

--- lib/get_it_context.dart
+++ lib/get_it_context.dart
@@ -2,6 +2,7 @@ import 'package:ceal_chronicler_f/incidents/model/incident_repository.dart';
 import 'package:get_it/get_it.dart';
 
 import 'commands/command_processor.dart';
+import 'io/file/file_adapter.dart';
 import 'io/repository_service.dart';
 import 'io/file/file_service.dart';
 import 'timeline/model/point_in_time_repository.dart';
@@ -13,6 +14,7 @@ void initializeGetItContext() {
   getIt.registerSingleton<IncidentRepository>(IncidentRepository());
 
   getIt.registerSingleton<RepositoryService>(RepositoryService());
+  getIt.registerSingleton<FileAdapter>(FileAdapter());
   getIt.registerSingleton<FileService>(FileService());
 
   getIt.registerSingleton<CommandProcessor>(CommandProcessor());
---
+++ test/get_it_context_test.dart
@@ -0,0 +1,11 @@
+import 'package:ceal_chronicler_f/get_it_context.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets(
+    'GetIt context can be initialized',
+    (WidgetTester tester) async {
+      initializeGetItContext();
+    },
+  );
+}
