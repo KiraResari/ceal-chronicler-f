0cc97dc594456b247e70ad3432d31b8b03d99652
Kira Resari <kira@tri-tail.com>
2024-12-27 10:00:31 +0100

    Extracted edit_button.dart from edit_location_level_button.dart

--- lib/locations/widgets/edit_location_level_button.dart
+++ lib/locations/widgets/edit_location_level_button.dart
@@ -1,37 +1,13 @@
-import 'package:flutter/material.dart';
-
-import '../../utils/widgets/buttons/small_circular_button.dart';
-import '../../utils/widgets/dialogs/select_dropdown_dialog.dart';
+import '../../utils/widgets/buttons/edit_button.dart';
 import '../model/location.dart';
 import '../model/location_level.dart';
 import 'edit_location_level_button_controller.dart';
 
-class EditLocationLevelButton extends SmallCircularButton {
-  final Location locationBeingEdited;
-  final EditLocationLevelButtonController controller;
-
-  EditLocationLevelButton(this.locationBeingEdited, {super.key})
-      : controller = EditLocationLevelButtonController(locationBeingEdited),
-        super(icon: Icons.edit);
-
-  @override
-  void onPressed(BuildContext context) async {
-    LocationLevel? selectedValue = await _showSelectionDialog(context);
-    controller.update(selectedValue);
-  }
-
-  Future<LocationLevel?> _showSelectionDialog(BuildContext context) {
-    List<DropdownMenuEntry<LocationLevel>> validEntries =
-        controller.validMenuEntries;
-    return showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return SelectDropdownDialog<LocationLevel>(
+class EditLocationLevelButton extends EditButton<LocationLevel> {
+  EditLocationLevelButton(Location locationBeingEdited, {super.key})
+      : super(
+          controller: EditLocationLevelButtonController(locationBeingEdited),
           initialSelection: locationBeingEdited.locationLevel,
-          entries: validEntries,
           labelText: "Select location level",
         );
-      },
-    );
-  }
 }
---
+++ lib/utils/widgets/buttons/edit_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../dialogs/select_dropdown_dialog.dart';
+import 'edit_button_controller.dart';
+import 'small_circular_button.dart';
+
+abstract class EditButton<T> extends SmallCircularButton {
+  final EditButtonController<T> controller;
+  final T initialSelection;
+  final String labelText;
+
+  const EditButton({
+    required this.controller,
+    required this.initialSelection,
+    required this.labelText,
+    super.key,
+  }) : super(icon: Icons.edit);
+
+  @override
+  void onPressed(BuildContext context) async {
+    T? selectedValue = await _showSelectionDialog(context);
+    controller.update(selectedValue);
+  }
+
+  Future<T?> _showSelectionDialog(BuildContext context) {
+    List<DropdownMenuEntry<T>> validEntries = controller.validMenuEntries;
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return SelectDropdownDialog<T>(
+          initialSelection: initialSelection,
+          entries: validEntries,
+          labelText: labelText,
+        );
+      },
+    );
+  }
+}
