20fa98f11c8388ae415073e2d295ea5017eec001
Kira Resari <kira@tri-tail.com>
2024-12-27 10:06:30 +0100

    Reformatting

--- lib/locations/widgets/edit_parent_location_button.dart
+++ lib/locations/widgets/edit_parent_location_button.dart
@@ -4,11 +4,10 @@ import '../model/location_id.dart';
 import 'edit_parent_location_button_controller.dart';
 
 class EditParentLocationButton extends EditButton<LocationId> {
-
   EditParentLocationButton(Location locationBeingEdited, {super.key})
       : super(
-      controller: EditParentLocationButtonController(locationBeingEdited),
-      initialSelection: locationBeingEdited.parentLocation,
-      labelText: "Select parent location",
-  );
+          controller: EditParentLocationButtonController(locationBeingEdited),
+          initialSelection: locationBeingEdited.parentLocation,
+          labelText: "Select parent location",
+        );
 }
