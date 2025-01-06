e94ff68813da018e5329036823378d248fddb99d
Kira Resari <kira@tri-tail.com>
2025-01-06 11:43:50 +0100

    Added move_attribute_up_command.dart

---
+++ lib/utils/commands/move_attribute_up_command.dart
@@ -0,0 +1,25 @@
+import '../../commands/command.dart';
+import '../list_utils.dart';
+import '../model/attribute.dart';
+import '../model/temporal_entity.dart';
+
+class MoveAttributeUpCommand extends Command {
+  final Attribute attribute;
+  final TemporalEntity entity;
+
+  MoveAttributeUpCommand(this.entity, this.attribute);
+
+  @override
+  void execute() =>
+      ListUtils.moveElementTowardsFrontOfList(entity.attributes, attribute);
+
+  @override
+  String get executeMessage => "Moved attribute ${attribute.name} up";
+
+  @override
+  void undo() =>
+      ListUtils.moveElementTowardsEndOfList(entity.attributes, attribute);
+
+  @override
+  String get undoMessage => "Moved attribute ${attribute.name} back down";
+}
